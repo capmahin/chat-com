5c0faaf0a0602f777c46a512b076a7b1d6cea7d7
Hawkeye <36890222+capmahin@users.noreply.github.com>
2023-06-09 03:18:04 +0600

    add variant

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,6 +1,11 @@
 'use client';
 
+import { useState } from "react";
+
+type Variant = 'LOGIN' | "REGISTER" ;
+
 const AuthFrom = () => {
+  const [variant, setVariant] = useState();
     return (
         <div>Auth From!</div>
       );
